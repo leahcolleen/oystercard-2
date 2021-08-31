234bc26d23e7167de2426955e377852141876b13
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-08-31 17:52:29 +0100

    Checking balance of a new card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require 'oystercard'
+
 describe Oystercard do
-  
+  subject (:oystercard) { described_class.new }
+
+  it 'checks new card has a balance' do
+    expect(oystercard.balance).to eq(0)
+  end
+
 end
