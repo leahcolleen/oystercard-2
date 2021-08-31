a275bdc661c7c590b02650c0e915978ece39089b
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-08-31 19:19:15 +0100

    Step 5: Top up oyster

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,6 +4,16 @@ class Oystercard
 
   def initialize 
     @balance = 0
+    # @count = []
   end
+
+  def top_up(value)
+    @balance += value
+  end
+
+  # def count
+  #   count += 1
+  #   @count << count
+  # end 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,10 @@ describe Oystercard do
     expect(oystercard.balance).to eq(0)
   end
 
-end
+  describe '#top_up(value)' do
+    it 'adds money to card' do
+      # expect(oystercard).to respond_to(:top_up).with(1).argument
+      expect { oystercard.top_up(1) }.to change { oystercard.balance }.by(1)
+    end 
+  end
+end        
